dd17a7e20927693a53405d19ac05fe9ad1503587
firdausi555 <sabilfirdausi80@gmail.com>
2025-04-25 03:41:32 +0530

    addedNewComponent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Greeting from './Greeting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Hey, I'm learning React! 💪</h1>
+      <Greeting name ="sabil"/>
+      <Greeting name="Alex" />
+      <Greeting name="Taylor" />
+      <Greeting name="Sky" />
+<p> asad asad asad asad asd
+  </p>    </div>
   );
 }
+
 
 export default App;
---
+++ src/Greeting.js
@@ -0,0 +1,9 @@
+function Greeting({name}){
+    return(
+        <div>
+            <h1>Namaste {name}</h1>
+        </div>
+
+    );
+}
+export default Greeting;
