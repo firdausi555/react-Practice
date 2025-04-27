1efdfc88885234bb163b7fe1a041f9be3cec0d54
firdausi555 <sabilfirdausi80@gmail.com>
2025-04-28 03:38:12 +0530

    anotherExampleForReducerHook

--- src/ReducerCounter.js
+++ src/ReducerCounter.js
@@ -29,3 +29,42 @@ function ReducerCounter() {
 }
 
 export default ReducerCounter;
+
+
+
+
+// Example =>2
+// import React, { useReducer } from 'react';
+
+// const initialState = { count: 0, message: "" };
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'increment':
+//       return { ...state, count: state.count + 1 };
+//     case 'decrement':
+//       return { ...state, count: state.count - 1 };
+//     case 'reset':
+//       return { ...state, count: 0 };
+//     case 'setMessage':
+//       return { ...state, message: action.payload };
+//     default:
+//       return state;
+//   }
+// }
+
+// function ReducerCounter() {
+//   const [state, dispatch] = useReducer(reducer, initialState);
+
+//   return (
+//     <div>
+//       <p>Count: {state.count}</p>
+//       <p>{state.message}</p>
+//       <button onClick={() => dispatch({ type: 'increment' })}>Increment</button>
+//       <button onClick={() => dispatch({ type: 'decrement' })}>Decrement</button>
+//       <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
+//       <button onClick={() => dispatch({ type: 'setMessage', payload: 'Hello, world!' })}>Set Message</button>
+//     </div>
+//   );
+// }
+// export default ReducerCounter;
